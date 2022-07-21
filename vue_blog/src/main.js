e4c09449634420d8933e1b3361827668802ff355
次林梦叶 <832230027@qq.com>
2022-07-21 23:25:11 +0800

    添加了vuex

--- vue_blog/src/main.js
+++ vue_blog/src/main.js
@@ -7,11 +7,14 @@ import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 import router from './router'
 
+import store from "./store/index";
+
 const app = createApp(App)
 
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
 }
 app.use(router);
-app.use(ElementPlus)
+app.use(ElementPlus);
+app.use(store);
 app.mount('#app')
